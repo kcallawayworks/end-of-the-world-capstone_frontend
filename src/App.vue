9e7eb8533e248a473089fb3112d2fce839c3b64c
callaway <kcallawayworks@gmail.com>
2019-03-12 12:57:17 -0600

    adding initial capstone frontend vue app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/signup">Signup</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/logout">Logout</router-link> 
     </div>
     <router-view/>
   </div>
@@ -10,7 +13,7 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
